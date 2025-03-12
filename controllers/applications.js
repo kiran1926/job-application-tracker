f187f956d76a25813d1636f89a3da15ccc7e32ae
kiran1926 <kiranroge19@gmail.com>
2025-03-12 17:26:18 -0500

    wip: completed index view, added new functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -7,7 +7,28 @@ const User = require("../models/user.js");
 // for applications index page 
 router.get("/", async(req, res) => {
     try{
-        res.render("applications/index.ejs");
+        const currentUser = await User.findById(req.session.user._id);
+
+        res.render("applications/index.ejs", {applications: currentUser.applications});
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
+    }
+});
+
+// view for creating new appln
+router.get("/new", async(req, res) => {
+    res.render("applications/new.ejs");
+});
+
+// post route for sending to db
+
+router.post("/", async(req, res) => {
+    try{
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
     } catch (error) {
         console.log(error);
         res.redirect("/");
