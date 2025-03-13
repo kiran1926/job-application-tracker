156fe8975bf37922d6f9b27b97b1f4a6228723c8
kiran1926 <kiranroge19@gmail.com>
2025-03-13 17:32:56 -0500

    added show, delete and edit functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -35,4 +35,59 @@ router.post("/", async(req, res) => {
     }
 });
 
+// show the specific application
+router.get("/:applicationId", async(req, res) => {
+    try{
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        res.render("applications/show.ejs", {
+            application: application, // property shorthand syntax 
+            // whenever same property name and variable name holding the value are the same
+            // write just {application}
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
+    }
+});
+
+// delete application
+router.delete("/:applicationId", async(req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.id(req.params.applicationId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
+    }
+});
+
+// show edit page form
+router.get("/:applicationId/edit", async(req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        res.render("applications/edit.ejs", {application});
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
+    }
+});
+
+// upadte edit 
+router.put("/:applicationId", async(req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        application.set(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications/${req.params.applicationId}`);
+    } catch (error) {
+        console.log(error);
+        res.redirect("/")
+    }
+})
+
 module.exports = router;
